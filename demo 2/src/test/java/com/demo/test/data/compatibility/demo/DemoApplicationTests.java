68d8f9927fbe52ef8442bc68caa09234af9ff58a
RostyslavPaliuha <rostyslavpaliuha@gmail.com>
2023-05-21 17:04:02 +0300

    fix failed build

--- demo 2/src/test/java/com/demo/test/data/compatibility/demo/DemoApplicationTests.java
+++ demo 2/src/test/java/com/demo/test/data/compatibility/demo/DemoApplicationTests.java
@@ -3,7 +3,6 @@ package com.demo.test.data.compatibility.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
